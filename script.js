749a5bb779f1e4e7fa6ec27dc641d60d5157f2f2
Karim <52040753+karimsiali@users.noreply.github.com>
2025-04-25 16:29:37 +0100

    Finish the assignment
    I finished the assignment with all functions working as intended.

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function getComputerChoice() {
     let random = Math.trunc(randomDecimal);
 
     if (random <= 33) {
-        return "Rock";
+        return "rock";
     } else if (random <= 67) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 
 }
@@ -17,8 +17,7 @@ function getComputerChoice() {
 function getHumanChoice() {
     
     let choice = prompt("Take your pick: ");
-    console.log(choice);
-    
+    return choice.toLocaleLowerCase();    
 }
 
 
@@ -26,18 +25,52 @@ function playGame() {
     let humanScore = 0;
     let computerSCore = 0;
 
-    // TODO
+    function increment(winner, humanChoice, computerChoice) {
+        if (winner == "human") {
+            console.log(`You win, ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        } else {
+            console.log(`You lose, ${computerChoice} beats ${humanChoice}`);
+            computerSCore++;
+        }
+    }
+    
     function playRound(humanChoice, computerChoice) {
         humanChoice = humanChoice.toLowerCase();
-        computerChoice = computerChoice.toLowerCase();
-
+        computerChoice = computerChoice;
         
+        if (humanChoice === computerChoice) {
+            console.log("It's a tie");
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+            increment("computer", "rock", "paper");
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            increment("human", "paper", "rock");
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            increment("human", "rock", "scissors");
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            increment("computer", "scissors", "rock");
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            increment("computer", "paper", "scissors");
+        } else {
+            increment("human", "scissors", "paper");
+        }
     }
 
     for (let i = 1; i <= 5; i++) {
-        playRound()
-        console.log(i);
+        playRound(getHumanChoice(), getComputerChoice())
     }
+
+    let gameWinner;
+    if (humanScore > computerSCore) {
+        gameWinner = "The Player";
+    } else if (computerSCore > humanScore) {
+        gameWinner = "Computer";
+    } else {
+        gameWinner = "Nobody, it's a tie";
+    }
+
+    console.log("The winner is: " + gameWinner)
+    
 }
 
 playGame();
